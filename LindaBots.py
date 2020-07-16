2b805f6acbdb3b98a1cee51c68ec20d9f2f9d0b4
dylan <temples@u.northwestern.edu>
2020-07-16 10:44:36 -0500

    add file

---
+++ LindaBots.py
@@ -0,0 +1,25 @@
+import numpy as np
+class GrumpySquidward:
+	
+	def evaluate_round(self, opponent_previous_choices):
+			return 1
+		
+	def __init__(self, pid=0):
+		self.name = "grumpy-squidward"
+		if (pid > 0):
+			self.name+"-"+str(pid)
+	
+
+class BandwagonerSpongebob:
+	
+	def evaluate_round(self, opponent_previous_choices):
+		 if (len(opponent_previous_choices)==0):
+			return 1
+		 else:
+			return sum(opponent_previous_choices) / 2 #chooses whatever is most popular with the opponent
+		
+	def __init__(self, pid=0):
+		self.name = "bandwagoner-spongebob"
+		if (pid > 0):
+			self.name+"-"+str(pid)
+
