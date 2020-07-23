5b183edb3416873ec19c9f5a7af830d79fa60c55
dylan <temples@u.northwestern.edu>
2020-07-23 11:29:33 -0500

    updates, add bots

--- PrisonersDilemma/DanBots.py
+++ PrisonersDilemma/DanBots.py
@@ -1,33 +1,33 @@
 import numpy as np
 
-class FlipACoin:	
-	def evaluate_round(self, Coin):
-		Coin = np.random.randint(0,100, size=1)
-		if (Coin >= 50):
-			return 0
-		else:
-			return 1	
+class FlipACoin:    
+    def evaluate_round(self, Coin):
+        Coin = np.random.randint(0,100, size=1)
+        if (Coin >= 50):
+            return 0
+        else:
+            return 1    
 
-	def __init__(self, pid=0):
-		self.name = "flip-a-coin"
-		if (pid > 0):
-			self.name = self.name + "-" + str(pid)
+    def __init__(self, pid=0):
+        self.name = "flip-a-coin"
+        if (pid > 0):
+            self.name = self.name + "-" + str(pid)
 
-class SICKO_MODE:	
-	def evaluate_round(self, opponent_previous_choices):
-		# If this is the first round, don't defect
-		if (len(opponent_previous_choices) == 0):
-			return 0
-		else:
-			if (sum(opponent_previous_choices) > 0):
-				return 1
-			else:
-				return 0	
+class SICKO_MODE:    
+    def evaluate_round(self, opponent_previous_choices):
+        # If this is the first round, don't defect
+        if (len(opponent_previous_choices) == 0):
+            return 0
+        else:
+            if (sum(opponent_previous_choices) > 0):
+                return 1
+            else:
+                return 0    
 
-	def __init__(self, pid=0):
-		self.name = "sicko-mode"
-		if (pid > 0):
-			self.name = self.name + "-" + str(pid)
+    def __init__(self, pid=0):
+        self.name = "sicko-mode"
+        if (pid > 0):
+            self.name = self.name + "-" + str(pid)
 
 class Lawful_Neutral:
     def evaluate_round(self, opponent_previous_choices):
@@ -55,7 +55,7 @@ class Chaotic_Neutral:
             self.name = self.name + "-" + str(pid)
 
 class Neutral_Evil:    
-	def evaluate_round(self, opponent_previous_choices):
+    def evaluate_round(self, opponent_previous_choices):
         opp_defected = (np.sum(opponent_previous_choices) > 0)
         if opp_defected or ((len(opponent_previous_choices) == 199) and (sum(opponent_previous_choices) == 0)):
             return 1
