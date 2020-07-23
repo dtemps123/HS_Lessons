e0389d0a5c1549cd504d30096f7c9e5c076da102
dylan <temples@u.northwestern.edu>
2020-07-23 11:26:03 -0500

    add bots

--- PrisonersDilemma/DanBots.py
+++ PrisonersDilemma/DanBots.py
@@ -28,3 +28,40 @@ class SICKO_MODE:
 		self.name = "sicko-mode"
 		if (pid > 0):
 			self.name = self.name + "-" + str(pid)
+
+class Lawful_Neutral:
+    def evaluate_round(self, opponent_previous_choices):
+        if (len(opponent_previous_choices) <= 10):
+            return 0
+        elif (np.sum(opponent_previous_choices[-10]) >5):
+            return 1
+        else:
+            return 0    
+    def __init__(self, pid=0):
+        self.name = "lawful-neutral"
+        if (pid > 0):
+            self.name = self.name + "-" + str(pid)
+
+class Chaotic_Neutral:
+    def evaluate_round(self, Chaos):
+        Chaos = np.random.randint(0,6, size=1)
+        if (Chaos % 2) ==0:
+            return 0
+        else:
+            return 1    
+    def __init__(self, pid=0):
+        self.name = "chaotic-neutral"
+        if (pid > 0):
+            self.name = self.name + "-" + str(pid)
+
+class Neutral_Evil:    
+	def evaluate_round(self, opponent_previous_choices):
+        opp_defected = (np.sum(opponent_previous_choices) > 0)
+        if opp_defected or ((len(opponent_previous_choices) == 199) and (sum(opponent_previous_choices) == 0)):
+            return 1
+        else:
+            return 0    
+    def __init__(self, pid=0):
+        self.name = "neutral-evil"
+        if (pid > 0): 
+            self.name = self.name+"-"+str(pid)
